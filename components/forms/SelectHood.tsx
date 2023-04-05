fa63ec2a0a5d3f1162c9bc08290fdb87fd622b3b
iiliev <iiliev@fxcm.com>
2023-04-05 16:18:34 +0300

    submiting the selected hoods

--- components/forms/SelectHood.tsx
+++ components/forms/SelectHood.tsx
@@ -1,66 +1,81 @@
 import { useState } from "react";
 import MultiSelect from "../multiSelect/MultiSelect";
 
-const DUMMY_DATA: Array<{ label: string, value: string, id: number }> = [ 
+// TODO: Find a better way to filter clicked items instead of using the selected boolean
+const DUMMY_DATA: DummyData[] = [ 
     {
         label: 'Манастирски Ливади',
         value: 'manLivadi',
-        id: 2
+        id: 2,
+        selected: false
     },
     {
         label: 'Хладилника',
         value: 'hladilnika',
-        id: 3 
+        id: 3,
+        selected: false 
     },
     {
         label: 'Редута',
         value: 'reduta',
-        id: 4 
+        id: 4,
+        selected: false
     },
     {
         label: 'Бояна',
         value: 'boyana',
-        id: 5
+        id: 5,
+        selected: false
     },
     {
         label: 'Борово',
         value: 'borovo',
-        id: 6 
+        id: 6,
+        selected: false
     } ];
 
+    export interface DummyData {
+        label: string;
+        value: string;
+        id: number;
+        selected: boolean;
+    }
+
 const SelectHood = (props: any) => {
-    const [ selectedHood, setSelectedHood ] = useState([]);
     const [ selected, setSelected ] = useState([]);
 
     // TODO: Fix this type
     const toggleOption = ({ id }: { id: never }) => {
         setSelected(prevSelected => {
-            const getItem: any = DUMMY_DATA.find( item => item.id === id);
-            console.log(getItem);
-            console.log(selected);
             // if it's in, remove
             const newArray = [...prevSelected]
             if (newArray.includes(id)) {
-                return newArray.filter(item => item != id)
+                return newArray.filter(item => item !== id);
                 // else, add
             } else {
                 newArray.push(id);
                 return newArray;
             }
-        })
+        });
+        
+        const clicked = selected.includes( id );
+
+        DUMMY_DATA.forEach( item => {
+            if (item.id === id) item.selected = !clicked;
+        } );
     }
     
     const handleSubmit = (event: any) => {
         event.preventDefault();
+        const submitSelected = DUMMY_DATA.filter( item => item.selected === true );
 
-        
         props.handleData({
-            selectedHood
+            submitSelected
         });
     }
     
     return (
-        <form onSubmit={handleSubmit}>
+        <form>
             <h1 className="text-lg mb-2">Изберете квартали в които ще работите</h1>
             {/* TODO: Add dynamic search
                 <div>
@@ -69,7 +84,7 @@ const SelectHood = (props: any) => {
              */}
             <MultiSelect options={DUMMY_DATA} selected={selected} toggleOption={toggleOption} />
             <div className="flex w-full">
-                <button disabled={ selected.join('').length === 0 } className={`bg-red-400 p-4 w-full text-white text-xl mt-4 rounded ${ selected.join('').length === 0 ? 'disabled' : ''}`}>Завърши регистрация</button>
+                <button onClick={handleSubmit} disabled={ selected.join('').length === 0 } className={`bg-red-400 p-4 w-full text-white text-xl mt-4 rounded ${ selected.join('').length === 0 ? 'disabled' : ''}`}>Завърши регистрация</button>
             </div>
         </form>
     )

32fac66f9a3ad6f4b420892bfc752721b9dbdb62
Viktor Georgiev <vgeorgiev1@icloud.com>
2023-04-25 17:03:28 +0300

    initial commmit

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -17,7 +17,6 @@ const Header = () => {
     const handleLogout = async () => {
         await signOut(auth);
         dispatch(storeActions.setUserLogin(false));
-        await signOut(auth);
     }
 
     return (

b3010d6604cf0b0703db49c0c39046711756c07b
Viktor Georgiev <vgeorgiev1@icloud.com>
2023-04-25 19:03:13 +0300

    master update

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -8,43 +8,48 @@ import { getStoreData } from "../forms/RegistrationComplete";
 import { signOut } from "firebase/auth";
 import { auth } from "../../firebase/config";
 import { storeActions } from "../../app/redux/store";
-import { useState } from "react";
 
 
 const Header = () => {
-    const getLoginState = useSelector<getStoreData>( state => state.dataStore.userLoggedin );
+    const getLoginState = useSelector<getStoreData>(state => state.dataStore.userLoggedin);
     const dispatch = useDispatch();
 
     const handleLogout = async () => {
+        await signOut(auth);
         dispatch(storeActions.setUserLogin(false));
     }
+
     return (
-        <nav className="flex items-center flex-row w-full max-w-6xl">
-            <div className="ml-0 mr-0"><Image src={logo} alt="pesitter logo" height="120" width="160" /></div>
-            <div className="flex ml-8 mr-0 grow justify-between items-center">
-                <div className="flex items-center ml-0 mr-0 text-lg">
-                    <Link href="/sitter" className="nav-link relative mx-2 group">Станете Гледач</Link>
-                    <Link href="/sitter" className="nav-link relative mx-2">Намерете Гледач</Link>
-                    <Link href="/sitter" className="nav-link relative mx-2">Контакти</Link>
-                    <Link href="/help" className="nav-link relative mx-2">Помощ</Link>
+        <header className="flex justify-center items-center mb-5 shadow-md w-full fixed  bg-white z-50 top-0">
+            <nav className="flex items-center flex-row w-full max-w-6xl">
+            <div className="ml-0 mr-0">
+                    <Link href="/"><Image src={logo} alt="pesitter logo" height="100" width="140" /></Link>
                 </div>
-                <div className="flex items-center mr-0 ml-2">
-                    {
-                        getLoginState ?
-                        <>
-                            <Link href="/" className="nav-link relative mx-2">Профил</Link>
-                            <button className="nav-link relative mx-2" onClick={handleLogout}>Изход</button>
-                        </>
-                        :
-                        <>
-                            <Link href="/register/regOptions" className="bg-slate-400 text-white px-4 py-2 rounded mr-5">Намерете работа</Link>
-                            <Link href="/login" className="nav-link relative mx-2">Вход</Link>
-                            <Link href="/register/regOptions" className="bg-red-500 text-white px-4 py-2 rounded ml-5">Регистрация</Link>
-                        </>
-                    }
+                <div className="flex ml-8 mr-0 grow justify-between items-center">
+                    <div className="flex items-center ml-0 mr-0 text-lg">
+                        <Link href="/sitter" className="nav-link relative mx-2 group">Станете Гледач</Link>
+                        <Link href="/findSitters" className="nav-link relative mx-2">Намерете Гледач</Link>
+                        <Link href="/sitter" className="nav-link relative mx-2">Контакти</Link>
+                        <Link href="/help" className="nav-link relative mx-2">Помощ</Link>
+                    </div>
+                    <div className="flex items-center mr-0 ml-2">
+                        {
+                            getLoginState ?
+                                <>
+                                    <Link href="/" className="nav-link relative mx-2">Профил</Link>
+                                    <button className="nav-link relative mx-2" onClick={handleLogout}>Изход</button>
+                                </>
+                                :
+                                <>
+                                    <Link href="/register/regOptions" className="bg-slate-400 text-white px-4 py-2 rounded mr-5">Намерете работа</Link>
+                                    <Link href="/login" className="nav-link relative mx-2">Вход</Link>
+                                    <Link href="/register/regOptions" className="bg-red-500 text-white px-4 py-2 rounded ml-5">Регистрация</Link>
+                                </>
+                        }
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </header>
     )
 }
 

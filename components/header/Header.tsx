a85301259eab22cce987c05884162ee212b1bdd9
Viktor Georgiev <vgeorgiev1@icloud.com>
2023-04-25 17:03:28 +0300

    initial commmit

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -8,43 +8,50 @@ import { getStoreData } from "../forms/RegistrationComplete";
 import { signOut } from "firebase/auth";
 import { auth } from "../../firebase/config";
 import { storeActions } from "../../app/redux/store";
+import { useState } from "react";
 
 
 const Header = () => {
-    const getLoginState = useSelector<getStoreData>( state => state.dataStore.userLoggedin );
+    const getLoginState = useSelector<getStoreData>(state => state.dataStore.userLoggedin);
     const dispatch = useDispatch();
 
     const handleLogout = async () => {
         dispatch(storeActions.setUserLogin(false));
         await signOut(auth);
     }
-
+    const [hideMenu, setHideMenu] = useState(true);
+    const showMenu = () => {
+        setHideMenu(state => !state)
+    }
     return (
-        <nav className="flex items-center flex-row w-full max-w-6xl">
+        <nav className="flex items-center sm:flex-row w-full max-w-6xl justify-between">
             <div className="ml-0 mr-0"><Image src={logo} alt="pesitter logo" height="120" width="160" /></div>
-            <div className="flex ml-8 mr-0 grow justify-between items-center">
-                <div className="flex items-center ml-0 mr-0 text-lg">
+            <div className={`${hideMenu ? 'hidden' : 'flex'} navi sm:flex sm:ml-8 sm:mr-0 flex-col sm:flex-row grow sm:items-center text-center justify-around`}>
+                <div className="flex items-center ml-0 mr-0 text-lg flex-col sm:flex-row">
                     <Link href="/sitter" className="nav-link relative mx-2 group">Станете Гледач</Link>
                     <Link href="/sitter" className="nav-link relative mx-2">Намерете Гледач</Link>
                     <Link href="/sitter" className="nav-link relative mx-2">Контакти</Link>
-                    <Link href="/sitter" className="nav-link relative mx-2">Помощ</Link>
+                    <Link href="/help" className="nav-link relative mx-2">Помощ</Link>
                 </div>
-                <div className="flex items-center mr-0 ml-2">
+                <div className="flex items-center sm:mr-0 sm:ml-2 flex-col sm:flex-row">
                     {
                         getLoginState ?
-                        <>
-                            <Link href="/" className="nav-link relative mx-2">Профил</Link>
-                            <button className="nav-link relative mx-2" onClick={handleLogout}>Изход</button>
-                        </>
-                        :
-                        <>
-                            <Link href="/register/regOptions" className="bg-slate-400 text-white px-4 py-2 rounded mr-5">Намерете работа</Link>
-                            <Link href="/login" className="nav-link relative mx-2">Вход</Link>
-                            <Link href="/register/regOptions" className="bg-red-500 text-white px-4 py-2 rounded ml-5">Регистрация</Link>
-                        </>
+                            <>
+                                <Link href="/" className="nav-link relative mx-2">Профил</Link>
+                                <button className="nav-link relative mx-2" onClick={handleLogout}>Изход</button>
+                            </>
+                            :
+                            <>
+                                <Link href="/register/regOptions" className="bg-slate-400 text-white px-4 py-2 rounded sm:mr-5">Намерете работа</Link>
+                                <Link href="/login" className="nav-link relative mx-2">Вход</Link>
+                                <Link href="/register/regOptions" className="bg-red-500 text-white px-4 py-2 rounded sm:ml-5">Регистрация</Link>
+                            </>
                     }
                 </div>
             </div>
+            <button className="mx-4 sm:hidden" onClick={showMenu}>
+                burger button
+            </button>
         </nav>
     )
 }

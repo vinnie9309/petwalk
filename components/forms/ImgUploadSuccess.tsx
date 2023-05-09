091aed68ce62840509d6ea6a825dd33f32866b0b
Ivaylo Iliev <ivaylogi@outlook.com>
2023-05-09 15:47:57 +0300

    text translation

--- components/forms/ImgUploadSuccess.tsx
+++ components/forms/ImgUploadSuccess.tsx
@@ -7,7 +7,7 @@ const ImgUploadSuccess = (props: any) => {
     return (
         <div>
             <h1 className="text-2xl text-center mb-5">Снимката изглежда супер!</h1>
-            <p>Thank you for making a good first impression! This photo will show up in search results, your profile page, and inbox.</p>
+            <p>Това е отличен начин да създадете добро първо впечатление. Снимката ви ще се показва в личният ви профил, резултатите от търсенето и във входящата поща.</p>
 
             <div className="flex w-full">
                 <button onClick={handleClick} className="bg-red-400 p-4 w-full text-white mt-4 rounded">Напред</button>

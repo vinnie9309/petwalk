5b165861720bf24a64f2dbff6ad2cad8e16b2c5a
iiliev <iiliev@fxcm.com>
2023-08-31 16:59:26 +0300

    adding hotjar script

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,6 +52,21 @@ export default function RootLayout({
             `
           }}
         ></Script>
+        <Script 
+          id="hotjar"
+          strategy='afterInteractive'
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(h,o,t,j,a,r){
+                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                  h._hjSettings={hjid:3635504,hjsv:6};
+                  a=o.getElementsByTagName('head')[0];
+                  r=o.createElement('script');r.async=1;
+                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                  a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `
+          }}></Script>
       </head>
       <body>
         <Providers>
